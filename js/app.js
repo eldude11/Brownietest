5fe3b52e70082fa6ff7a6f767315c0c6a1dd5297
Dav3DArt <51569088+eldude11@users.noreply.github.com>
2022-03-25 18:24:01 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/BrownieAR.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Brownie AR.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
